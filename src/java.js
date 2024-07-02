8d8b7e95eca8f293779d1a7c16f0aaf5391b8c94
Vasa Vanisri Naga Ratna Tejaswini <109909104+20A31A0526@users.noreply.github.com>
2024-07-02 21:13:03 +0530

    Add files via upload

---
+++ src/java.js
@@ -0,0 +1,246 @@
+function openBiryaniRecipePopup() {
+  var biryaniPopup = document.getElementById("recipePopup");
+  biryaniPopup.style.display = "block";
+}
+
+function openPaneerRecipePopup() {
+  var paneerPopup = document.getElementById("paneerRecipePopup");
+  paneerPopup.style.display = "block";
+}
+
+function openTandooriRecipePopup() {
+  var tandooriPopup = document.getElementById("tandooriRecipePopup");
+  tandooriPopup.style.display = "block";
+}
+
+function openMushroomRecipePopup() {
+  var mushroomPopup = document.getElementById("mushroomRecipePopup");
+  mushroomPopup.style.display = "block";
+}
+
+function openNaanRecipePopup() {
+  var naanPopup = document.getElementById("naanRecipePopup");
+  naanPopup.style.display = "block";
+}
+
+function openDalRecipePopup() {
+  var dalPopup = document.getElementById("dalRecipePopup");
+  dalPopup.style.display = "block";
+}
+
+function openSambarRecipePopup() {
+  var sambarPopup = document.getElementById("sambarRecipePopup");
+  sambarPopup.style.display = "block";
+}
+
+function openGulabRecipePopup() {
+  var gulabPopup = document.getElementById("gulabRecipePopup");
+  gulabPopup.style.display = "block";
+}
+
+function openSamosaRecipePopup() {
+  var samosaPopup = document.getElementById("SamosaRecipePopup");
+  samosaPopup.style.display = "block";
+}
+
+function openmirchibajjiRecipePopup() {
+  var mirchibajjiPopup = document.getElementById("mirchibajjiRecipePopup");
+  mirchibajjiPopup.style.display = "block";
+}
+
+function openpakodiRecipePopup() {
+  var pakodiPopup = document.getElementById("pakodiRecipePopup");
+  pakodiPopup.style.display = "block";
+}
+
+function opennoodlesRecipePopup() {
+  var noodlesPopup = document.getElementById("noodlesRecipePopup");
+  noodlesPopup.style.display = "block";
+}
+
+function openpavbajjiRecipePopup() {
+  var pavbajjiPopup = document.getElementById("pavbajjiRecipePopup");
+  pavbajjiPopup.style.display = "block";
+}
+
+function openburgerRecipePopup() {
+  var burgerPopup = document.getElementById("burgerRecipePopup");
+  burgerPopup.style.display = "block";
+}
+
+function openpizzaRecipePopup() {
+  var pizzaPopup = document.getElementById("pizzaRecipePopup");
+  pizzaPopup.style.display = "block";
+}
+
+function openvegmanchuriaRecipePopup() {
+  var vegmanchuriaPopup = document.getElementById("vegmanchuriaRecipePopup");
+  vegmanchuriaPopup.style.display = "block";
+}
+
+function openMangoJuiceRecipePopup() {
+  var mangoJuicePopup = document.getElementById("mangoJuiceRecipePopup");
+  mangoJuicePopup.style.display = "block";
+}
+
+function openLassiRecipePopup() {
+  var lassiPopup = document.getElementById("lassiRecipePopup");
+  lassiPopup.style.display = "block";
+}
+
+function opendalgonaCoffeeRecipePopup() {
+  var dalgonaCoffeePopup = document.getElementById("dalgonaCoffeeRecipePopup");
+  dalgonaCoffeePopup.style.display = "block";
+}
+
+function openorangejuiceRecipePopup() {
+  var orangeJuicePopup = document.getElementById("orangeJuiceRecipePopup");
+  orangeJuicePopup.style.display = "block";
+}
+
+function openteaRecipePopup() {
+  var teaPopup = document.getElementById("teaRecipePopup");
+  teaPopup.style.display = "block";
+}
+
+function openbadamMilkRecipePopup() {
+  var badamMilkPopup = document.getElementById("badamMilkRecipePopup");
+  badamMilkPopup.style.display = "block";
+}
+
+function opencoffeeRecipePopup() {
+  var coffeePopup = document.getElementById("coffeeRecipePopup");
+  coffeePopup.style.display = "block";
+}
+
+function openbananSmoothieRecipePopup() {
+  var coffeePopup = document.getElementById("bananaSmoothieRecipePopup");
+  coffeePopup.style.display = "block";
+}
+
+
+// Close the recipe popups when clicking outside the content area
+window.onclick = function(event) {
+  var biryaniPopup = document.getElementById("recipePopup");
+  var paneerPopup = document.getElementById("paneerRecipePopup");
+  var tandooriPopup = document.getElementById("tandooriRecipePopup");
+  var mushroomPopup = document.getElementById("mushroomRecipePopup");
+  var naanPopup = document.getElementById("naanRecipePopup");
+  var dalPopup = document.getElementById("dalRecipePopup");
+  var sambarPopup = document.getElementById("sambarRecipePopup");
+  var gulabPopup = document.getElementById("gulabRecipePopup");
+  var samosaPopup = document.getElementById("SamosaRecipePopup");
+  var mirchibajjiPopup = document.getElementById("mirchibajjiRecipePopup");
+  var pakodiPopup = document.getElementById("pakodiRecipePopup");
+  var noodlesPopup = document.getElementById("noodlesRecipePopup");
+  var pavbajjiPopup = document.getElementById("pavbajjiRecipePopup");
+  var burgerPopup = document.getElementById("burgerRecipePopup");
+  var pizzaPopup = document.getElementById("pizzaRecipePopup");
+  var vegmanchuriaPopup = document.getElementById("vegmanchuriaRecipePopup");
+  var mangoJuicePopup = document.getElementById("mangoJuiceRecipePopup");
+  var lassiPopup = document.getElementById("lassiRecipePopup");
+  var dalgonaCoffeePopup = document.getElementById("dalgonaCoffeeRecipePopup");
+  var orangePopup = document.getElementById("orangeJuiceRecipePopup");
+  var teaPopup = document.getElementById("teaRecipePopup");
+  var badamPopup = document.getElementById("badamMilkRecipePopup");
+  var bananPopup = document.getElementById("bananaSmoothieRecipePopup");
+  var coffeePopup = document.getElementById("coffeeRecipePopup");
+
+
+  if (event.target == biryaniPopup) {
+    biryaniPopup.style.display = "none";
+  }
+
+  if (event.target == paneerPopup) {
+    paneerPopup.style.display = "none";
+  }
+
+  if (event.target == tandooriPopup) {
+    tandooriPopup.style.display = "none";
+  }
+
+  if (event.target == mushroomPopup) {
+    mushroomPopup.style.display = "none";
+  }
+
+  if (event.target == naanPopup) {
+    naanPopup.style.display = "none";
+  }
+
+  if (event.target == dalPopup) {
+    dalPopup.style.display = "none";
+  }
+
+  if (event.target == sambarPopup) {
+    sambarPopup.style.display = "none";
+  }
+
+  if (event.target == gulabPopup) {
+    gulabPopup.style.display = "none";
+  }
+
+  if (event.target == samosaPopup) {
+    samosaPopup.style.display = "none";
+  }
+
+  if (event.target == mirchibajjiPopup) {
+    mirchibajjiPopup.style.display = "none";
+  }
+
+  if (event.target == pakodiPopup) {
+    pakodiPopup.style.display = "none";
+  }
+
+  if (event.target == noodlesPopup) {
+    noodlesPopup.style.display = "none";
+  }
+
+  if (event.target == pavbajjiPopup) {
+    pavbajjiPopup.style.display = "none";
+  }
+
+  if (event.target == burgerPopup) {
+    burgerPopup.style.display = "none";
+  }
+
+  if (event.target == pizzaPopup) {
+    pizzaPopup.style.display = "none";
+  }
+
+  if (event.target == vegmanchuriaPopup) {
+    vegmanchuriaPopup.style.display = "none";
+  }
+
+  if (event.target == mangoJuicePopup) {
+    mangoJuicePopup.style.display = "none";
+  }
+
+  if (event.target == lassiPopup) {
+    lassiPopup.style.display = "none";
+  }
+
+  if (event.target == dalgonaCoffeePopup) {
+    dalgonaCoffeePopup.style.display = "none";
+  }
+
+  if (event.target == orangePopup) {
+    orangePopup.style.display = "none";
+  }
+
+  if (event.target == teaPopup) {
+    teaPopup.style.display = "none";
+  }
+
+  if (event.target == badamPopup) {
+    badamPopup.style.display = "none";
+  }
+
+  if (event.target == bananPopup) {
+    bananPopup.style.display = "none";
+  }
+  if (event.target == coffeePopup) {
+    coffeePopup.style.display = "none";
+  }
+
+
+};
